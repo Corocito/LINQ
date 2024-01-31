70f04f2c9a86e58bcd24119f9220264123b601f7
Corozco <orozcomondragons@gmail.com>
2024-01-31 17:23:15 -0500

    Se añaden los metodos para mostrar la información de cada uno de los operadores de LINQ

--- Program.cs
+++ Program.cs
@@ -16,7 +16,47 @@ using LINQ;
 
 LinqQuery query = new LinqQuery();
 
-var libros= query.TodalaColeccion();
+//MOSTRAR CONDICION ALL
+// var libros= query.ValorEnStatus();
+// Console.WriteLine($"Todos los libros poseen estatus?: {libros}");
+// //-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// //MOSTRAR CONDICION ANY
+// var algunoPublicado2005 = query.AlgunoPublicado2005();
+// Console.WriteLine($"Algun libro fue publicado en 2005?: {algunoPublicado2005}");
 
-query.ShowValues(libros);
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//MOSTRAR CONDICION WHERE
+// var mostrarResultados = query.LibrosDespues2000();
+// query.ShowValues(mostrarResultados);
 
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//MOSTRAR CONDICIÓN CONTAINS
+// var MostrarContains = query.ContienePython();
+// query.ShowValues(MostrarContains);
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//MOSTRAR CONDICION ORDERBY
+// var MostrarResultadoOrdenarJava = query.OrdenarJavaTitulo();
+// query.ShowValues(MostrarResultadoOrdenarJava);
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//MOSTRAR CONDICION ORDERBYDESCENDING
+// var MostrarPorPaginasDesc = query.OrderDescending();
+// query.ShowValues(MostrarPorPaginasDesc);
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//MOSTRAR OPERADOR TAKE
+// var mostrarTake = query.OperadorTake();
+// query.ShowValues(mostrarTake);
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//MOSTRAR OPERADOR SKIP
+// var mostrarSkip = query.operadorSkip();
+// query.ShowValues(mostrarSkip);
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//MOSTRAR OPERADOR SELECT
+// var mostrarSelect = query.operadorSelect();
+// query.ShowValues(mostrarSelect);
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//MOSTRAR OPERADOR COUNT
+Console.WriteLine($"Cantidad de libros que tienen entre 200 y 500 páginas: {query.operadorCount()}");
