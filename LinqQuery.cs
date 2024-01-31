c6c0fabc24f0376be5319abb4edba446646c58d2
Corozco <orozcomondragons@gmail.com>
2024-01-31 09:26:17 -0500

    Se crea el constructor para los metodos de LINQ

---
+++ LinqQuery.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    public class LinqQuery
+    {
+        //Se crea el constructor para la clase
+        public LinqQuery(){
+
+        }
+    }
+}
